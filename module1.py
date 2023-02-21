6373c3bbb92861dfef5d64227c78cad0561d3c46
opilane <opilane@HP-CZC2349HTP>
2023-02-21 14:06:48 +0200

    Add project files.

---
+++ module1.py
@@ -0,0 +1,13 @@
+﻿def leia_samad_palgad(palgad, inimesed):
+    palgad_counts = []      #count/числа
+    for i in range(len(palgad)):       #присваивает переменной значение/ повторяя код(используя Len-(Возвращает количество символов или же зарплату))
+        if palgad[i] in palgad_counts:        
+            palgad_counts[palgad[i]].append(inimesed[i])
+        else:
+            palgad_counts[i] = [inimesed[i]]
+    for palgad in palgad_counts:
+        if len(palgad_counts[palgad]) > 1:
+            print(f"Järgmistel {len(palgad_counts[palgad])} inimestel on palk {palgad}: {','.join(palgad_counts[palgad])}")
+
+
+           
---
+++ palgad.py
@@ -0,0 +1,9 @@
+﻿from math import*
+from random import*
+from module1 import*
+
+
+palgad = [1200,2500,750,395,1200]
+inimesed = ["A","B","C","D","E"]
+
+leia_samad_palgad(palgad, inimesed)
