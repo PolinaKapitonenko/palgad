a2aeb77bfb0444b068831b18e4594f768a95c2bd
opilane <opilane@HP-CZC2349HTP>
2023-02-27 12:04:44 +0200

    .

--- module1.py
+++ module1.py
@@ -1,13 +1,26 @@
-﻿def leia_samad_palgad(palgad, inimesed):
-    palgad_counts = []      #count/числа
-    for i in range(len(palgad)):       #присваивает переменной значение/ повторяя код(используя Len-(Возвращает количество символов или же зарплату))
-        if palgad[i] in palgad_counts:        
+﻿#6
+def leia_samad_palgad(palgad, inimesed):
+    palgad_counts = {}  
+    for i in range(len(palgad)):
+        if palgad[i] in palgad_counts:
             palgad_counts[palgad[i]].append(inimesed[i])
         else:
-            palgad_counts[i] = [inimesed[i]]
-    for palgad in palgad_counts:
-        if len(palgad_counts[palgad]) > 1:
-            print(f"Järgmistel {len(palgad_counts[palgad])} inimestel on palk {palgad}: {','.join(palgad_counts[palgad])}")
+            palgad_counts[palgad[i]] = [inimesed[i]]
+    for palganumber, inimesed in palgad_counts.items():
+        if len(inimesed) > 1:
+           print(f"Järgmistel {len(palgad_counts[palgad])} inimestel on palk {palgad}: {','.join(palgad_counts[palgad])}")
 
 
-           
+
+#16
+def nimeta_iga_kolmanda_inimene_ümber(inimesed, uued_nimed):
+    uuendatud_inimesed = []
+    for i, nimi in (inimesed):
+        if (i+1) % 3 == 0:  #  3
+            if uued_nimed:  # если остались новые имена
+                uuendatud_inimesed.append(uued_nimed.pop(0))
+            else:
+                uuendatud_inimesed.append(nimi)  # использовать новое имя
+        else:
+            uuendatud_inimesed.append(nimi)
+    return uuendatud_inimesed
--- palgad.py
+++ palgad.py
@@ -2,8 +2,23 @@
 from random import*
 from module1 import*
 
+#16 number
+palgad = [1200, 2500, 750, 395, 1200]
+inimesed = ["A", "B", "C", "D", "E"]
+
+uuendatud_inimesed = []
+for i in range(len(inimesed)):
+    if (i+1) % 3 == 0:
+        uued_nimed = input(f"Sisestage uus nimi {inimesed[i]}: ")
+        uuendatud_inimesed.append(uuendatud_inimesed)
+
+uuendatud_inimesed ,uuendatud_inimesed = nimeta_iga_kolmanda_inimene_ümber(inimesed, uued_nimed)
+print(uuendatud_inimesed)
+
+#6 number
 
 palgad = [1200,2500,750,395,1200]
 inimesed = ["A","B","C","D","E"]
 
 leia_samad_palgad(palgad, inimesed)
+
